baf2bf6537f0f50121fea04d496f36b5da6d287e
cfq <754944486@qq.com>
2017-12-12 16:06:02 +0800

    删除

--- my_demo_pro/src/main/java/com/cfq/demo/util/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.cfq.demo.util;
-
-public class Constants {
-	/**项目内统一的NameSpace定义.**/  
-    /**wsdl2java生成客户端代码时默认按定义的namespace倒序生成包路径 **/  
-    public static final String NS = "http://soap.webservice.dmis.icsshs.com";
-}
